c14f74c93e78b7446e4cc7d24f6ddd8a766b7acb
Augustin Moses.R <augustin.moses@azimpremjifoundation.org>
2026-04-13 15:47:48 +0530

    fix:Unit wise v10

--- annual_budget/api/foundation_consolidated_report.py
+++ annual_budget/api/foundation_consolidated_report.py
@@ -2170,7 +2170,256 @@ def get_unit_wise_plan(financial_year, month, table_name_filter=None):
 
     return final_results
 
+@frappe.whitelist(allow_guest=True)
+def get_foundation_overall(financial_year, month, table_name_filter=None):
 
+    def safe_join(arr):
+        return ",".join([str(x).strip() for x in (arr or []) if x])
+
+    # ---------------- TOTAL CALCULATIONS ----------------
+    def calculate_totals(actuals):
+
+        for head in actuals:
+
+            total_ytd = 0
+            total_actual = 0
+
+            if head.get("sub_heads"):
+
+                for sub in head.get("sub_heads", []):
+
+                    sub_ytd = 0
+                    sub_actual = 0
+
+                    for item in sub.get("items", []):
+                        sub_ytd += item.get("ytd", 0) or 0
+                        sub_actual += item.get("total_posted_amt", 0) or 0
+
+                    sub["ytd"] = round(sub_ytd, 2)
+                    sub["total_posted_amt_ytd"] = round(sub_actual, 2)
+
+                    total_ytd += sub_ytd
+                    total_actual += sub_actual
+
+            else:
+                for item in head.get("items", []):
+                    total_ytd += item.get("ytd", 0) or 0
+                    total_actual += item.get("total_posted_amt", 0) or 0
+
+            head["ytd"] = round(total_ytd, 2)
+            head["total_posted_amt_ytd"] = round(total_actual, 2)
+
+        return actuals
+
+    def calculate_grand_total(actuals):
+
+        grand_ytd = sum(h.get("ytd", 0) or 0 for h in actuals)
+        grand_actual = sum(h.get("total_posted_amt_ytd", 0) or 0 for h in actuals)
+
+        return {
+            "grand_total_ytd": round(grand_ytd, 2),
+            "grand_total_actual": round(grand_actual, 2)
+        }
+
+    # ---------------- CONSOLIDATION ----------------
+    def consolidate_actuals(all_unit_actuals):
+
+        head_map = {}
+
+        for unit_data in all_unit_actuals:
+
+            for head in unit_data:
+
+                key = head["name"]
+
+                if key not in head_map:
+                    head_map[key] = {
+                        "name": head["name"],
+                        "sequence_id": head["sequence_id"],
+                        "sub_heads": [],
+                        "items": [],
+                        "ytd": 0,
+                        "total_posted_amt_ytd": 0
+                    }
+
+                head_map[key]["ytd"] += head.get("ytd", 0)
+                head_map[key]["total_posted_amt_ytd"] += head.get("total_posted_amt_ytd", 0)
+
+                # ---- SUB HEADS ----
+                if head.get("sub_heads"):
+
+                    sub_map = {s["name"]: s for s in head_map[key]["sub_heads"]}
+
+                    for sub in head.get("sub_heads", []):
+
+                        if sub["name"] not in sub_map:
+                            new_sub = {
+                                "name": sub["name"],
+                                "sequence_id": sub["sequence_id"],
+                                "items": [],
+                                "ytd": 0,
+                                "total_posted_amt_ytd": 0
+                            }
+                            head_map[key]["sub_heads"].append(new_sub)
+                            sub_map[sub["name"]] = new_sub
+
+                        sub_map[sub["name"]]["ytd"] += sub.get("ytd", 0)
+                        sub_map[sub["name"]]["total_posted_amt_ytd"] += sub.get("total_posted_amt_ytd", 0)
+
+                        item_map = {i["name"]: i for i in sub_map[sub["name"]]["items"]}
+
+                        for item in sub.get("items", []):
+
+                            if item["name"] not in item_map:
+                                new_item = {
+                                    "name": item["name"],
+                                    "sequence_id": item["sequence_id"],
+                                    "gl_code": item.get("gl_code"),
+                                    "ytd": 0,
+                                    "total_posted_amt": 0
+                                }
+                                sub_map[sub["name"]]["items"].append(new_item)
+                                item_map[item["name"]] = new_item
+
+                            item_map[item["name"]]["ytd"] += item.get("ytd", 0)
+                            item_map[item["name"]]["total_posted_amt"] += item.get("total_posted_amt", 0)
+
+                # ---- DIRECT ITEMS ----
+                else:
+                    item_map = {i["name"]: i for i in head_map[key]["items"]}
+
+                    for item in head.get("items", []):
+
+                        if item["name"] not in item_map:
+                            new_item = {
+                                "name": item["name"],
+                                "sequence_id": item["sequence_id"],
+                                "gl_code": item.get("gl_code"),
+                                "ytd": 0,
+                                "total_posted_amt": 0
+                            }
+                            head_map[key]["items"].append(new_item)
+                            item_map[item["name"]] = new_item
+
+                        item_map[item["name"]]["ytd"] += item.get("ytd", 0)
+                        item_map[item["name"]]["total_posted_amt"] += item.get("total_posted_amt", 0)
+
+        return list(head_map.values())
+
+    # ---------------- MAIN LOGIC ----------------
+
+    previous_financial_year = get_previous_financial_year(financial_year)
+
+    settings = get_combination_table_settings(table_name_filter)
+    settings = sorted(settings, key=lambda x: x.get("settings_doc", ""))
+
+    final_results = []
+
+    # periods
+    current_formatted = get_accounting_period_from_month(month, financial_year)
+    prev_formatted = get_accounting_period_from_month(month, previous_financial_year)
+
+    current_grouped = get_grouped_actuals(
+        fiscal_year=current_formatted.get("fiscal_year"),
+        accounting_period=current_formatted.get("accounting_period")
+    ).get("data", [])
+
+    prev_grouped = get_grouped_actuals(
+        fiscal_year=prev_formatted.get("fiscal_year"),
+        accounting_period=prev_formatted.get("accounting_period")
+    ).get("data", [])
+
+    # ---------------- LOOP ----------------
+    for s in settings:
+
+        units = safe_join(s.get("units"))
+        cost_centers = safe_join(s.get("cost_centers"))
+        locations = safe_join(s.get("locations"))
+        cost_centers_erp = safe_join(s.get("cost_centers_erp"))
+        locations_erp = safe_join(s.get("locations_erp"))
+
+        for combo in s.get("combination_settings", []):
+
+            # -------- CURRENT YEAR --------
+            all_actuals = []
+
+            actuals = get_combined_actuals(
+                financial_year=financial_year,
+                month=month,
+                unit=units,
+                cost_center=cost_centers,
+                location_code=locations,
+                erp_cost_center_value=cost_centers_erp,
+                erp_loc_value=locations_erp,
+                grouped_actuals_data=current_grouped
+            )
+
+            actuals = calculate_totals(actuals)
+            all_actuals.append(actuals)
+
+            actuals = consolidate_actuals(all_actuals)
+            totals = calculate_grand_total(actuals)
+
+            actuals.append({
+                "name": "GRAND TOTAL",
+                "sequence_id": 9999,
+                "sub_heads": [],
+                "items": [],
+                "ytd": totals["grand_total_ytd"],
+                "total_posted_amt_ytd": totals["grand_total_actual"]
+            })
+
+            # -------- PREVIOUS YEAR --------
+            all_prev_actuals = []
+
+            previous_actuals = get_combined_actuals(
+                financial_year=previous_financial_year,
+                month=month,
+                unit=units,
+                cost_center=cost_centers,
+                location_code=locations,
+                erp_cost_center_value=cost_centers_erp,
+                erp_loc_value=locations_erp,
+                grouped_actuals_data=prev_grouped
+            )
+
+            previous_actuals = calculate_totals(previous_actuals)
+            all_prev_actuals.append(previous_actuals)
+
+            previous_actuals = consolidate_actuals(all_prev_actuals)
+            previous_totals = calculate_grand_total(previous_actuals)
+
+            previous_actuals.append({
+                "name": "GRAND TOTAL",
+                "sequence_id": 9999,
+                "sub_heads": [],
+                "items": [],
+                "ytd": previous_totals["grand_total_ytd"],
+                "total_posted_amt_ytd": previous_totals["grand_total_actual"]
+            })
+
+            # -------- FINAL RESULT --------
+            final_results.append({
+                "settings_doc": s.get("settings_doc"),
+                "label": s.get("label"),
+
+                "table_name": combo.get("table_name"),
+                "sequence_id": combo.get("sequence_id"),
+                "is_this_sub_item": combo.get("is_this_sub_item"),
+
+                "units": units,
+                "cost_centers": cost_centers,
+                "locations": locations,
+                "cost_centers_erp": cost_centers_erp,
+                "locations_erp": locations_erp,
+
+                "actuals": actuals,
+                "previous_actuals": previous_actuals
+            })
+
+    final_results = sorted(final_results, key=lambda x: x.get("sequence_id", 0))
+
+    return final_results
 
 # @frappe.whitelist(allow_guest=True)
 # def get_(financial_year, month, table_name_filter=None):
@@ -2325,6 +2574,7 @@ def get_unit_wise_plan(financial_year, month, table_name_filter=None):
 #     final_results = sorted(final_results, key=lambda x: x.get("sequence_id", 0))
 
 #     return final_results
+
 
 
 import frappe
